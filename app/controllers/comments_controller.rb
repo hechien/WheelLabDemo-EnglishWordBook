65dec4f9878b3beed4660d2945b918f7ed2734ec
HeChien Hsu <hechien@me.com>
2013-06-29 01:28:29 +0800

    建立起新增Comment時會有的動作。 `current_user.comments` 代表的是登入的使用者底下的comments collection。 透過`current_user.comments.build(params[:comment])`來在當前登入的使用者之下的comments集合中建立一筆Comment暫存資料，等到save的時候就會儲存到資料庫中。 如果儲存成功的話就會把畫面轉回到單字頁面，如果失敗的話資料不會儲存，但是一樣是顯示單字頁面，但是會保留輸入的評論內容。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+
+    @word = Word.find(params[:word_id])
+
+    @comment = current_user.comments.build(params[:comment])
+    @comment.word = @word
+    if @comment.save
+      redirect_to word_path(@word)
+    else
+      render 'word/show'
+    end
+  end
 end
